409cbcd52ed0711b7e534eef1857fdb76d23db8f
Ryan Ma <lunaticasian1@gmail.com>
2023-09-11 16:37:43 -0600

    1 macro conditionals

--- include/uart_hello.h
+++ include/uart_hello.h
@@ -2,10 +2,14 @@
 
 #ifdef TESTING_ENV
 struct device {};
+
+void test_uart_out();
+void test_uart_in();
+
 #else
 #include <drivers/uart.h>
+
 #endif
 
 void echo_uppercase(const struct device *dev);
-void test_uart_out();
-void test_uart_in();
+
--- src/uart_hello.c
+++ src/uart_hello.c
@@ -7,15 +7,30 @@ void echo_uppercase(const struct device *dev)
     char byte, up;
 
     do {
+        #ifdef TESTING_ENV
+            test_uart_in();
+        {
+
+        }
+        #else
         if (uart_poll_in(dev, &byte) != 0) {
             continue;
         }
+        #endif
 
+        
         if (byte <= 'z' && byte >= 'a')
             up = byte - 'a' + 'A';
+
         else
             up = byte;
 
-        uart_poll_out(dev, up);
+
+        #ifdef TESTING_ENV
+            test_uart_out();
+        #else
+            uart_poll_out(dev, up);
+
+        #endif
     } while(byte != '\n');
 }

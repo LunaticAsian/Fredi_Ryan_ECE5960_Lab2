c8a27e3edbd6ddb141f5cb2163bdf18ed1755d60
Fredi R. Mino <35937021+Fredware@users.noreply.github.com>
2023-09-13 16:36:14 -0600

    Indented some code

--- src/uart_hello.c
+++ src/uart_hello.c
@@ -1,36 +1,33 @@
 #include "uart_hello.h"
 
-
-//need to fix file path for uart hello include header file
 void echo_uppercase(const struct device *dev)
 {
     char byte, up;
 
-    do {
-        #ifdef TESTING_ENV
-            test_uart_in();
+    do
+    {
+#ifdef TESTING_ENV
+        if (test_uart_in() != 0)
         {
-
-        }
-        #else
-        if (uart_poll_in(dev, &byte) != 0) {
             continue;
         }
-        #endif
+#else
+        if (uart_poll_in(dev, &byte) != 0)
+        {
+            continue;
+        }
+#endif
 
-        
         if (byte <= 'z' && byte >= 'a')
             up = byte - 'a' + 'A';
 
         else
             up = byte;
 
-
-        #ifdef TESTING_ENV
-            test_uart_out();
-        #else
-            uart_poll_out(dev, up);
-
-        #endif
-    } while(byte != '\n');
+#ifdef TESTING_ENV
+        test_uart_out();
+#else
+        uart_poll_out(dev, up);
+#endif
+    } while (byte != '\n');
 }
